f0b7bf2d3fb95a250da674b833d09c8cd2e2018f
Jenish Vaghela <116751887+Jenish-v@users.noreply.github.com>
2026-08-12 19:25:22 -0400

    Isolate the application smoke test

--- backend/src/test/java/com/jenish/smartparking/SmartParkingApplicationTest.java
+++ backend/src/test/java/com/jenish/smartparking/SmartParkingApplicationTest.java
@@ -3,9 +3,10 @@ package com.jenish.smartparking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.autoconfigure.exclude="
-        + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
-        + "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration")
+@SpringBootTest(properties = {
+        "spring.flyway.enabled=false",
+        "spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration"
+})
 class SmartParkingApplicationTest {
 
     @Test
